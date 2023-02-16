5691d728a381d96ae356e1ec9292ba753eae3af3
misternooblet <dragonroadsupport@gmail.com>
2023-02-16 18:55:10 +0200

    basic login error

--- src/components/LoginComponents/Input.js
+++ src/components/LoginComponents/Input.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './assets/styles/Input.module.css'
-import { CiUser, CiLock } from 'react-icons/ci';
+import { CiMail, CiLock } from 'react-icons/ci';
 
 
 const Input = ({ type, name, inputHandler, value }) => {
@@ -8,7 +8,7 @@ const Input = ({ type, name, inputHandler, value }) => {
         <div className={styles.input_container}>
             <p>{name}</p>
             <div className={styles.input_row}>
-                <span>{name === 'email' ? <CiUser /> : <CiLock />}</span>
+                <span>{name === 'email' ? <CiMail /> : <CiLock />}</span>
                 <input type={type} id={name} name={name} value={value} onChange={(e) => inputHandler(e, name)}></input>
             </div>
         </div>
--- src/components/LoginComponents/LoginForm.js
+++ src/components/LoginComponents/LoginForm.js
@@ -12,6 +12,7 @@ const initialLoginState = {
 
 const LoginForm = () => {
     const [loginInput, setLoginInput] = useState(initialLoginState)
+    const [loginFailed, setLoginFailed] = useState(false)
 
     const handleLoginInput = (e, name) => {
         const value = e.target.value
@@ -23,13 +24,19 @@ const LoginForm = () => {
         const username = loginInput.email
         const password = loginInput.password
         const user = users.find(element => element.email === username && element.password === password)
-        return user ? localStorage.setItem('user', JSON.stringify(user)) : null
+        if (user) {
+            localStorage.setItem('user', JSON.stringify(user))
+            setLoginFailed(false)
+        } else {
+            setLoginFailed(true)
+        }
 
     }
     return (
         <div className={styles.loginCard}>
             <img src={logoimg} alt='Logo'></img>
             <h1>Login:</h1>
+            {loginFailed && <h4 className={styles.error_msg}>Login Failed check your login details</h4>}
             <Input type={'email'} name={'email'} inputHandler={handleLoginInput} value={loginInput.email} />
             <Input type={'password'} name={'password'} inputHandler={handleLoginInput} value={loginInput.password} />
             <button onClick={handleLoginButton}>Login</button>
