8b5a1810cdf139c7bc45a30d8a75a3691eb30cbc
misternooblet <dragonroadsupport@gmail.com>
2023-02-17 01:11:45 +0200

    votes adding up

--- src/components/VotingComponents/Card.js
+++ src/components/VotingComponents/Card.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from './assets/styles/Card.module.css'
-const Card = ({ name, image, votes }) => {
+const Card = ({ name, image, votes, voteHandler }) => {
     return (
         <div className={styles.card}>
             <img src={image} alt={name} />
-            <h3 className={styles.card_name}>{name}</h3>
-            <button className={styles.card_btn}>Vote</button>
-            <div className={styles.card_stats}></div>
+            <h3 className={styles.card_name}>{name} <span className={styles.card_stats}>{votes} / 7</span></h3>
+            <button className={styles.card_btn} onClick={() => { voteHandler(name) }}>Vote</button>
         </div>
     )
 }
--- src/components/VotingComponents/CardsContainer.js
+++ src/components/VotingComponents/CardsContainer.js
@@ -4,12 +4,20 @@ import styles from './assets/styles/CardsContainer.module.css'
 
 const parties = JSON.parse(localStorage.getItem('parties'))
 const user = JSON.parse(localStorage.getItem('user'))
-
+const votesData = parties.reduce((a, b) => {
+    let name = b.name
+    a[name] = 0
+    return a
+}, {})
 const CardsContainer = () => {
+    const [votes, setVotes] = useState(votesData)
+    const voteHandler = (name) => {
+        setVotes({ ...votes, [name]: (votes[`${name}`] + 1) })
+    }
     return (
         <div className={styles.cards_container}>
             <h3>Voter:{user.name}</h3>
-            {parties.map((element, idx) => { return <Card key={idx} name={element.name} image={element.image} votes={element.votes} /> })}
+            {parties.map((element, idx) => { return <Card key={idx} name={element.name} image={element.image} votes={votes[element.name]} voteHandler={voteHandler} /> })}
         </div>
     )
 }
