1c0a8709c8e2d3974c34b653e986686398335b3a
misternooblet <dragonroadsupport@gmail.com>
2023-02-16 17:39:42 +0200

    testing netlify

--- src/components/LoginComponents/LoginForm.js
+++ src/components/LoginComponents/LoginForm.js
@@ -3,12 +3,12 @@ import styles from './assets/styles/LoginForm.module.css'
 import logoimg from './assets/images/logo.png'
 import Input from './Input'
 
-const users = JSON.parse(localStorage.getItem('users'))
+// const users = JSON.parse(localStorage.getItem('users'))
 
 const LoginForm = () => {
     return (
         <div className={styles.loginCard}>
-            <img src={logoimg}></img>
+            <img src={logoimg} alt='Logo'></img>
             <h1>Login:</h1>
             <Input type={'text'} name={'Username'} />
             <Input type={'password'} name={'Password'} />
