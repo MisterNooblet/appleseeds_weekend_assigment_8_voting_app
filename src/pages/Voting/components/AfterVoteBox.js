81bc7c94b25be5a4da6162fcbf3b6892315d7eb2
misternooblet <dragonroadsupport@gmail.com>
2023-02-17 15:16:32 +0200

    vote handler function added

--- src/pages/Voting/components/AfterVoteBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AfterVoteBox = () => {
-    return (
-        <div>AfterVoteBox</div>
-    )
-}
-
-export default AfterVoteBox
--- src/pages/Voting/components/CardsContainer.js
+++ src/pages/Voting/components/CardsContainer.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react'
 import Card from './Card'
 import styles from './assets/styles/CardsContainer.module.css'
+import LSM from '../../../utils/LocalStorageManager'
 
 const CardsContainer = () => {
-    const parties = JSON.parse(localStorage.getItem('parties'))
-    const user = JSON.parse(localStorage.getItem('user'))
-    const votesData = parties.reduce((a, b) => {
-        let name = b.name
-        a[name] = 0
-        return a
-    }, {})
-    const [votes, setVotes] = useState(votesData)
+    const parties = LSM.pull('parties')
+    const user = LSM.pull('user')
+    const [votes, setVotes] = useState(parties)
+    LSM.push('votes', votes)
     const voteHandler = (name) => {
-        setVotes({ ...votes, [name]: (votes[`${name}`] + 1) })
+        // eslint-disable-next-line
+        setVotes(votes.map(element => element.name === name ? { ...element, ['votes']: element['votes'] + 1 } : element))
     }
     return (
         <div className={styles.cards_container}>
             <h3>Voter:{user.name}</h3>
-            {parties.map((element, idx) => { return <Card key={idx} name={element.name} image={element.image} votes={votes[element.name]} voteHandler={voteHandler} /> })}
+            {votes.map((element, idx) => { return <Card key={idx} name={element.name} image={element.image} votes={element.votes} voteHandler={voteHandler} /> })}
         </div>
     )
 }
