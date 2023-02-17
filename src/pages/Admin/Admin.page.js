d4f586389e5ca6655d76ad8190659f79cd35744b
misternooblet <dragonroadsupport@gmail.com>
2023-02-17 18:53:45 +0200

    admin panel logout button

--- src/pages/Admin/Admin.page.js
+++ src/pages/Admin/Admin.page.js
@@ -4,13 +4,18 @@ import LSM from '../../utils/LocalStorageManager'
 import AdminTitle from './components/AdminTitle'
 import TotalVotes from './components/TotalVotes'
 import UsersTable from './components/UsersTable'
-const Admin = () => {
+import Button from '../../components/Button/Button'
+const Admin = ({ pageManager }) => {
     const user = LSM.pull('user')
     return (
         <Wrapper>
             <AdminTitle name={user.name} />
             <UsersTable />
             <TotalVotes />
+            <Button content={'LOGOUT'} onClick={() => {
+                LSM.logout()
+                pageManager('Login')
+            }} />
         </Wrapper>
     )
 }
