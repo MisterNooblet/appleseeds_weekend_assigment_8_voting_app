8a94710d4138940c4338ced4c297440c5ae9cfde
misternooblet <dragonroadsupport@gmail.com>
2023-02-17 16:03:57 +0200

    vote + revote

--- src/pages/Voting/components/Card.js
+++ src/pages/Voting/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './assets/styles/Card.module.css'
 import Button from '../../../components/Button/Button'
-const Card = ({ name, image, votes, voteHandler }) => {
+const Card = ({ name, image, votes, modalHandler }) => {
     return (
         <div className={styles.card}>
             <img src={image} alt={name} />
             <h3 className={styles.card_name}>{name} <span className={styles.card_stats}>{votes} / 7</span></h3>
-            <Button onClick={() => { voteHandler(name) }} content='Vote' />
+            <Button onClick={() => { modalHandler(name, 'vote') }} content='Vote' />
         </div>
     )
 }
--- src/pages/Voting/components/CardsContainer.js
+++ src/pages/Voting/components/CardsContainer.js
@@ -2,22 +2,52 @@ import React, { useState } from 'react'
 import Card from './Card'
 import styles from './assets/styles/CardsContainer.module.css'
 import LSM from '../../../utils/LocalStorageManager'
+import Modal from '../../../components/Modal/Modal'
+import Button from '../../../components/Button/Button'
 
 const CardsContainer = () => {
     const parties = LSM.pull('parties')
     const user = LSM.pull('user')
     const [votes, setVotes] = useState(parties)
+    const [modalActive, setModalActive] = useState(false)
     LSM.push('votes', votes)
     const voteHandler = (name) => {
         // eslint-disable-next-line
+        LSM.push('tempvote', votes)
         setVotes(votes.map(element => element.name === name ? { ...element, ['votes']: element['votes'] + 1 } : element))
     }
-    return (
-        <div className={styles.cards_container}>
-            <h3>Voter:{user.name}</h3>
-            {votes.map((element, idx) => { return <Card key={idx} name={element.name} image={element.image} votes={element.votes} voteHandler={voteHandler} /> })}
-        </div>
-    )
+    const modalHandler = (name, action) => {
+        if (action === 'vote' && !user.voted) {
+            user.voted = true
+            LSM.push('user', user)
+            setTimeout(() => { setModalActive(true) }, 2000)
+
+            voteHandler(name)
+        } else if (action === 'revote') {
+            const previousState = LSM.pull('tempvote')
+            user.voted = false
+            LSM.push('user', user)
+            setModalActive(false)
+            setVotes(previousState)
+        }
+    }
+    if (modalActive) {
+        return (
+            <Modal content={`Thank you for your participation ${user.name}, you can still change your vote or confirm and logout`}>
+                <Button content={'Re-vote'} onClick={() => modalHandler(null, 'revote')}></Button>
+                <Button content={'Confirm & log out'}></Button>
+            </Modal>
+        )
+    } else if (!modalActive) {
+        return (
+            <div className={styles.cards_container}>
+                <h3>Voter:{user.name}</h3>
+                {votes.map((element, idx) => { return <Card key={idx} name={element.name} image={element.image} votes={element.votes} modalHandler={modalHandler} /> })}
+            </div>
+        )
+
+    }
+
 }
 
 export default CardsContainer
