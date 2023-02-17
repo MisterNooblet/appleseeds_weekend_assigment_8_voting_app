6c763844b9fa498bacb197be554dfbd8beaf8b38
misternooblet <dragonroadsupport@gmail.com>
2023-02-17 13:24:09 +0200

    fixed warnings for netlify

--- src/pages/Login/Login.page.js
+++ src/pages/Login/Login.page.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import LoginForm from './components/LoginForm'
 import Wrapper from '../../components/PageWrapperCol'
-import Modal from '../../components/Modal/Modal'
-import Button from '../../components/Button/Button'
+
 
 const Login = ({ pageManager }) => {
 
